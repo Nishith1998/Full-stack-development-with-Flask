c72e324277b304127609f224000e512a2ab21016
Nishith1998 <nishithgoswami23@gmail.com>
2020-06-17 18:56:52 +0530

    add models for user, course and enrollment

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,7 @@ from flask import render_template
 from flask import request
 from flask import Response
 from flask import json
-
+from application.models import User, Course, Enrollment
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
 @app.route("/")
@@ -48,13 +48,6 @@ def apifun(index=None):
         jdata = courseData[int(index)]
     
     return Response(json.dumps(jdata), mimetype="application/json")
-
-class User(db.Document):
-    user_id     =   db.IntField(unique = True)
-    first_name  =   db.StringField(max_length = 50)
-    last_name   =   db.StringField(max_length = 50)
-    email       =   db.StringField(max_length = 30)
-    password    =   db.StringField(max_length = 30)
 
 @app.route("/user")
 def userfun():
